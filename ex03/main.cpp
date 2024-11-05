b6095455ea31cfb22965a1718a6f35b7faf1a0cd
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-11-05 16:35:13 +0100

    Update main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,5 +1,12 @@
 #include "Character.hpp"
+#include "Cure.hpp"
+#include "Ice.hpp"
 
 int main(void) {
-	Character xav;
+	Character xav("Xavier");
+
+	xav.equip(new Ice);
+	xav.equip(new Cure);
+	xav.unequip(1);
+	xav.unequip(1);
 }
